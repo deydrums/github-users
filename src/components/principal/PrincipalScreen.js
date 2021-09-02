66ebcc4ace559023df8401d13474521225593916
deydrums <dagarcia100@gmail.com>
2021-09-02 14:42:12 -0600

    data

--- src/components/principal/PrincipalScreen.js
+++ src/components/principal/PrincipalScreen.js
@@ -44,7 +44,25 @@ export const PrincipalScreen = () => {
                 </div>
 
                 <div className ="p__box-c p__box-3">
-                    <div className = "p__box-3-c"></div>
+                    <div className = "p__box-3-c">
+                        <div className = "p__box-3-container">
+                            <div className = "p__box-3-a">
+                                <div className = "p_box-img">
+                                    <div className = "p__img-cont">
+
+                                    </div>
+                                </div>
+                                <div className = "p_box-usr-d">
+                                    <h1>The Octocat</h1>
+                                    <h2>@octocat</h2>
+                                    <h3>Unido 25 Junio 2011</h3>
+                                </div>
+                            </div>
+                            <div className = "p__box-3-b">
+                                
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
