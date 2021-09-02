78316d5ac97dd8db735ec4238380914ca0de204c
deydrums <dagarcia100@gmail.com>
2021-09-02 17:32:07 -0600

    uiReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,11 @@
 //******************* rootReducer ******************* */
 
 import {combineReducers} from 'redux';
+import { uiReducer } from './uiReducer';
 import { userReducer } from './userReducer';
 
 
 export const rootReducer = combineReducers({
     user: userReducer,
+    ui: uiReducer
 });
---
+++ src/reducers/uiReducer.js
@@ -0,0 +1,16 @@
+//******************* uiReducer ******************* */
+
+
+import { types } from "../types/types";
+
+
+const initialState = {
+    fetch: false,
+}
+
+export const uiReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
