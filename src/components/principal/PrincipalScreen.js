0e6dac5a6200e539dee4686515d7adcf1fce7e07
deydrums <dagarcia100@gmail.com>
2021-09-02 11:48:15 -0600

    principal

--- src/components/principal/PrincipalScreen.js
+++ src/components/principal/PrincipalScreen.js
@@ -2,8 +2,24 @@ import React from 'react'
 
 export const PrincipalScreen = () => {
     return (
-        <div className = 'principal__container'>
-            Principal Screen
+        <div className = 'p__container'>
+            <div className = 'p__box'>
+                <div className ="p__box-c p__box-1">
+                    <div className = "p__box-1-c">
+                        <h2>GitHubUsers</h2>
+                        <h3>LIGHT</h3>
+                    </div>
+                </div>
+
+                <div className ="p__box-c p__box-2">
+                    <div className = "p__box-2-c"></div>
+                </div>
+
+                <div className ="p__box-c p__box-3">
+                    <div className = "p__box-3-c"></div>
+                </div>
+
+            </div>
         </div>
     )
 }
